19379a0188f94b436aa7ad7d5ad3622619403a8e
Austin Musiku <47279609+MusikuAustin@users.noreply.github.com>
2023-06-16 21:08:24 +0300

    refactor form dropdown to separate file

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,41 @@
 import { useState, useEffect } from "react";
 import Mps from "./MpsList";
 import mpArray from "./mps";
+import FormDropdown from "./FormDropdown";
 
 const App = () => {
 	const [firstLoad, setFirstLoad] = useState(true);
 
-    // text inputs
+	// text inputs
 	const [name, setName] = useState("");
-    const [constituency, setConstituency] = useState("");
+	const [constituency, setConstituency] = useState("");
 
-    // dropdowns
+	// dropdowns
 	const [party, setParty] = useState("");
 	const [county, setCounty] = useState("");
 
-    // checkboxes
-    const [present, setPresent] = useState(null);
-    const [vote, setVote] = useState(null);
+	// checkboxes
+	const [present, setPresent] = useState(null);
+	const [vote, setVote] = useState(null);
 
 	const [filteredMps, setFilteredMps] = useState([]);
 
-    let mps = mpArray;
+	let mps = mpArray;
 
 	useEffect(() => {
 		setFilteredMps(filterMps(mps));
 	}, [name, party, county, constituency, present, vote]);
 
-
 	function filterMps(mps) {
-		if (name === "" && party === "" && county === "" && constituency === "" && present === null && vote === null) return mps;
+		if (
+			name === "" &&
+			party === "" &&
+			county === "" &&
+			constituency === "" &&
+			present === null &&
+			vote === null
+		)
+			return mps;
 
 		if (name !== "") {
 			mps = mps.filter((item) =>
@@ -40,12 +48,12 @@ const App = () => {
 		}
 
 		if (county !== "") {
-            mps = mps.filter((item) => item[2] === county);
-        }
+			mps = mps.filter((item) => item[2] === county);
+		}
 
-        if (constituency !== "") {
-            mps = mps.filter((item) => item[3] === constituency);
-        }
+		if (constituency !== "") {
+			mps = mps.filter((item) => item[3] === constituency);
+		}
 
 		return mps;
 	}
@@ -57,16 +65,18 @@ const App = () => {
 		setConstituency("");
 		setPresent(null);
 		setVote(null);
-		setFilteredMps(mps); 
+		setFilteredMps(mps);
 	}
 
 	return (
 		<div className="search-params">
 			<div className="mb-8 bg-slate-900 px-8 pb-8 pt-8">
-				<h1 className="mb-8 font-display text-5xl text-white">Players</h1>
+				<h1 className="mb-8 font-sans text-3xl text-white">
+					Finance Bill 2023 votes
+				</h1>
 
 				<form className="grid w-full grid-cols-7 flex-row items-end gap-2">
-                    {/* mp name */}
+					{/* mp name */}
 					<div className="input-container col-start-1 col-end-3 flex flex-col items-start gap-[0.125rem]">
 						<label className="text-white" htmlFor="player">
 							Search by mp's name
@@ -82,104 +92,45 @@ const App = () => {
 					</div>
 
 					{/* party */}
-					<div className="input-container col-start-5 col-end-6 flex flex-col items-start gap-[0.125rem]">
-						<label className="text-sm text-white" htmlFor="position">
-							Filter by party
-						</label>
-						<select
-							id="party"
-							className="block w-full border-none bg-[#141f39] p-2 text-sm text-[#f2f2f2]"
+					<div className="col-start-5 col-end-6">
+						<FormDropdown
+							label="party"
+							options={mps.map((item) => item[4])}
 							value={party}
 							onChange={(e) => setParty(e.target.value)}
-						>
-							<option value="">All</option>
-							{mps
-								.map((item) => item[4])
-								.filter((value, index, self) => self.indexOf(value) === index && value !== ""  && value !== "More...")
-								.sort()
-								.map((item, index) => {
-									return (
-										<option value={item} key={index}>
-											{item}
-										</option>
-									);
-								})
-								
-							}
-						</select>
+						></FormDropdown>
 					</div>
 
-                    {/* county */}
-					<div className="input-container col-start-6 col-end-7 flex flex-col items-start gap-[0.125rem]">
-						<label className="text-sm text-white" htmlFor="position">
-							Filter by county
-						</label>
-						<select
-							id="county"
-							className="block w-full border-none bg-[#141f39] p-2 text-sm text-[#f2f2f2]"
+					{/* county */}
+					<div className="col-start-6 col-end-7">
+						<FormDropdown
+							label="county"
+							options={mps.map((item) => item[2])}
 							value={county}
 							onChange={(e) => setCounty(e.target.value)}
-						>
-							<option value="">All</option>
-							{mps
-								.map((item) => item[2])
-								.filter((value, index, self) => {
-									return value !== "" && self.indexOf(value) === index  
-								})
-								.sort()
-								.map((item, index) => {
-									console.log('pre-filtered', item)
-									return (
-										<option value={item} key={index}>
-											{item}
-										</option>
-									);
-								})
-								
-							}
-						</select>
-					</div>
-                    
-                    {/* constituency */}
-					<div className="input-container col-start-7 col-end-8 flex flex-col items-start gap-[0.125rem]">
-						<label className="text-sm text-white" htmlFor="position">
-							Filter by constituency
-						</label>
-						<select
-							id="constituency"
-							className="block w-full border-none bg-[#141f39] p-2 text-sm text-[#f2f2f2]"
-							value={constituency}
-							onChange={(e) => setConstituency(e.target.value)}
-						>
-							<option value="">All</option>
-                                {mps
-                                    .map((item) => item[3])
-                                    .filter((value, index, self) => self.indexOf(value) === index && value !== "")
-									.sort()
-                                    .map((item, index) => {
-                                        return (
-                                            <option value={item} key={index}>
-                                                {item}
-                                            </option>
-                                        );
-                                    })
-                                    
-                                }
-						</select>
+						></FormDropdown>
 					</div>
 
-                    
+					{/* constituency */}
+					<div className="col-start-7 col-end-8">
+						<FormDropdown
+							label="constituency"
+							options={mps.map((item) => item[3])}
+							value={constituency}
+							onChange={(e) => setConstituency(e.target.value)}
+						></FormDropdown>
+					</div>
 				</form>
 			</div>
 
 			{filteredMps.length === 0 ? (
 				<div className="flex flex-col items-center justify-center gap-4">
-					<h1 className="text-3xl text-center">Oops, no mps found!</h1>
-					<button 
+					<h1 className="text-center text-3xl">Oops, no mps found!</h1>
+					<button
 						onClick={resetFilters}
-						className="underline text-blue-500 hover:text-blue-700"
+						className="text-blue-500 underline hover:text-blue-700"
 					>
-							Reset Filters
+						Reset Filters
 					</button>
 				</div>
 			) : (
---
+++ src/FormDropdown.jsx
@@ -0,0 +1,31 @@
+const FormDropdown = ({ label, options, value, onChange }) => {
+	return (
+		<div className="input-container flex flex-col items-start gap-[0.125rem]">
+			<label className="text-white" htmlFor="player">
+				{label}
+			</label>
+			<select
+				id="name"
+				className="block w-full border-none bg-[#141f39] p-2 text-sm text-[#f2f2f2]"
+				value={value}
+				onChange={onChange}
+			>
+				<option value="">All</option>
+				{options
+					.filter((value, index, self) => self.indexOf(value) === index && value !== ""  && value !== "More...")
+					.sort()
+					.map((item, index) => {
+						return (
+							<option value={item} key={index}>
+								{item}
+							</option>
+						);
+					})
+					
+				}
+			</select>
+		</div>
+	);
+};
+
+export default FormDropdown
