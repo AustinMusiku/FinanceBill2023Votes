df5fe78fb2b265e02f665f88673609a2f70f6c16
Austin Musiku <47279609+MusikuAustin@users.noreply.github.com>
2023-06-18 05:01:39 +0300

    add vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { inject } from "@vercel/analytics";
 import App from "./App";
+
+inject();
 
 ReactDOM.createRoot(document.getElementById("main-content")).render(
 	<BrowserRouter>
